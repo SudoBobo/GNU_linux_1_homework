cf7366266cc80ce1cc85473de60b5b9dcc45c646
Ivan Koptelov <ivan.koptelov@tarantool.org>
2018-09-23 19:18:59 +0300

    Initial commit

---
+++ 1/README.md
@@ -0,0 +1,4 @@
+#Usage
+```bash
+./iso_creator.sh -i iso_image_name -d dir_name -u user_name -g group_name
+```
---
+++ 1/iso_creator.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+#set -u
+
+OK_MSG="done"
+ERR_MSG="error"
+
+# Defaults
+ISO_NAME="iso_image"
+DIR="test_dir1"
+
+# Reading named args
+while getopts i:d:u:g: option
+do
+case "${option}"
+in
+i) ISO_NAME=${OPTARG};;
+d) DIR=${OPTARG};;
+u) I_USER=${OPTARG};;
+g) GROUP=${OPTARG};;
+esac
+done
+
+# Here we could check args (for example, check that given DIR exists and
+# current user has -r access to it) But as task says "print 'error' for all errors"
+# we don't do it. If something would go wrong, mkisofs will return non-zero.
+
+
+# run the command that do the job
+#mkisofs -J -o $ISO_NAME $DIR &>/dev/null
+mkisofs -J -o $ISO_NAME $DIR
+if [[ $? != 0 ]]
+then
+    echo $ERR_MSG && exit 1
+fi
+
+# change user and group of resulting file
+if [ -n "${I_USER}" ];
+then
+    chown $I_USER $ISO_NAME &>/dev/null
+fi
+
+if [ -n "${GROUP}" ];
+then
+    chown $I_USER:$GROUP $ISO_NAME &>/dev/null
+fi
+
+# check user/group changing for errors
+if [[ $? != 0 ]]
+then
+    echo $ERR_MSG && exit 1
+fi
+
+echo $OK_MSG && exit 0
